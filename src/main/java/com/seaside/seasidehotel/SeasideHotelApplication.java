9969fdce45e44c47ce33719da63d6f6f214eba15
SabaMaghradze <sabamaghradze1001@gmail.com>
2025-01-04 14:05:26 +0400

    basic setup

---
+++ src/main/java/com/seaside/seasidehotel/SeasideHotelApplication.java
@@ -0,0 +1,13 @@
+package com.seaside.seasidehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeasideHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeasideHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seaside/seasidehotel/SeasideHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seaside.seasidehotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeasideHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
