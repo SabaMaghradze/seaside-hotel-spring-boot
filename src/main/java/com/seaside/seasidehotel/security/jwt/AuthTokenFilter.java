7388da61c8e8fb24bca8317a6f5b7394591f0286
Saba Maghradze <sabamaghradze1001@gmail.com>
2025-09-27 17:55:50 +0400

    small changes

--- src/main/java/com/seaside/seasidehotel/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/seaside/seasidehotel/security/jwt/AuthTokenFilter.java
@@ -1,11 +1,9 @@
 package com.seaside.seasidehotel.security.jwt;
-import com.seaside.seasidehotel.security.user.UserDtls;
 import com.seaside.seasidehotel.security.user.UserDtlsService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
